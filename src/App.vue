c227a53cb832145067a5e3d65a24ead795e3069f
DiioBrando <olsaf21f12@gmail.com>
2025-08-14 21:30:31 +0500

    feat: add Header component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,5 @@
 </script>
 
 <template>
-  <div class="flex gap-2 w-full">
-    <RouterLink to="/">Main</RouterLink>
-    <RouterLink to="/incomes">Incomes</RouterLink>
-    <RouterLink to="/orders">Orders</RouterLink>
-    <RouterLink to="/sales">Sales</RouterLink>
-    <RouterLink to="/stocks">Stocks</RouterLink>
-  </div>
   <RouterView/>
 </template>
---
+++ src/features/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const navRoutes = [
+  {
+    name: 'incomes',
+    path: '/incomes',
+
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+
+  },
+  {
+    path: '/sales',
+    name: 'sales',
+  },
+  {
+    path: '/stocks',
+    name: 'stocks',
+  },
+];
+</script>
+
+<template>
+  <header class="h-[80px] bg-black flex items-center">
+    <nav class="mx-auto flex w-full max-w-[1200px] px-3 sm:px-5">
+      <ul class="flex gap-4">
+        <li v-for="route in navRoutes" :key="route.name">
+          <RouterLink
+              :to="route.path"
+              class="px-4 py-2 rounded-md text-white hover:bg-gray-700 transition-colors duration-200"
+          >
+            {{ route.name }}
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
