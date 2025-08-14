f041f2550e0ccaeaa8f06b06077a848666fe02c6
DiioBrando <olsaf21f12@gmail.com>
2025-08-14 19:18:31 +0500

    feat: PrimeVue, and routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from "./router/router.ts";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app
+    .use(router)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    })
+    .mount('#app');
