4916331ae3ec90183602e3149a43f63e5f0117c9
DiioBrando <olsaf21f12@gmail.com>
2025-08-14 21:31:20 +0500

    feat: nested routes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,44 +1,46 @@
-import Main from "../features/Main.vue";
 import Incomes from "../features/pages/Incomes.vue";
 import Orders from "../features/pages/Orders.vue";
 import Sales from "../features/pages/Sales.vue";
 import Stocks from "../features/pages/Stocks.vue";
+import Main from "../features/Main.vue";
 import {createRouter, createWebHashHistory} from "vue-router";
 
 const routes = [
     {
         path: '/',
-        component: Main,
         name: 'Main',
-    },
-    {
-        path: '/incomes',
-        name: 'incomes',
-        component: Incomes,
+        redirect: '/incomes',
+        component: Main,
+        children: [
+            {
+                path: 'incomes',
+                name: 'incomes',
+                component: Incomes,
 
-    },
-    {
-        path: '/orders',
-        name: 'orders',
-        component: Orders,
+            },
+            {
+                path: 'orders',
+                name: 'orders',
+                component: Orders,
 
-    },
-    {
-        path: '/sales',
-        name: 'sales',
-        component: Sales,
-    },
-    {
-        path: '/stocks',
-        name: 'stocks',
-        component: Stocks,
+            },
+            {
+                path: 'sales',
+                name: 'sales',
+                component: Sales,
+            },
+            {
+                path: 'stocks',
+                name: 'stocks',
+                component: Stocks,
+            },
+        ],
     },
     {
         path: '/:catchAll(.*)',
         redirect: '/'
     },
-]
-;
+];
 
 const router = createRouter({
     routes,
